37f7a4cbeb6ae05ab5611ca864dd7cae1b556734
SemperParatusGithub <schlagernils@gmail.com>
2020-12-14 23:35:37 +0100

    Added Texture Type Coverter Functions

--- SemperEngine/src/SemperEngine/Renderer/Backend/OpenGL/GLTools.h
+++ SemperEngine/src/SemperEngine/Renderer/Backend/OpenGL/GLTools.h
@@ -4,6 +4,8 @@
 
 namespace SemperEngine
 {
+	using RendererID = uint32_t;
+
 	// Forward Declarations
 	class VertexBuffer;
 	enum class VertexFormat;
@@ -11,10 +13,10 @@ namespace SemperEngine
 	class GLIndexBuffer;
 	enum class IndexFormat;
 
-	enum class BufferUsage;
+	enum class TextureWrap;
+	enum class TextureFormat;
 
-	// alias for uint32_t
-	using RendererID = uint32_t;
+	enum class BufferUsage;
 
 	class GLTools
 	{
@@ -23,5 +25,10 @@ namespace SemperEngine
 		static uint32_t IndexFormatToGLType(IndexFormat format);
 
 		static uint32_t BufferUsageGLUsage(BufferUsage usage);
+
+		static uint32_t TextureWrapToGL(TextureWrap wrap);
+		static uint32_t TextureFormatToGL(TextureFormat format);
+		static uint32_t TextureFormatToInternalFormat(uint32_t format);
+		static TextureFormat BitsToTextureFormat(uint32_t bits);
 	};
 }
