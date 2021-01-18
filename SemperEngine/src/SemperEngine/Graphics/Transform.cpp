e1b78394927bf11db68edf8ffead6fb05c726a04
SemperParatusGithub <schlagernils@gmail.com>
2021-01-18 11:06:20 +0100

    added transform component

--- SemperEngine/src/SemperEngine/Graphics/Transform.cpp
+++ SemperEngine/src/SemperEngine/Graphics/Transform.cpp
@@ -1,7 +1,9 @@
 #include "Precompiled.h"
 #include "Transform.h"
 
+#include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/quaternion.hpp>
 
 
 namespace SemperEngine
@@ -63,19 +65,11 @@ namespace SemperEngine
 	{
 		m_TransformationMatrix = Mat4(1.0f);
 
-		if(m_Translation != Vec3(0.0f))
-			m_TransformationMatrix *= glm::translate(Mat4(1.0f), m_Translation);
+		m_TransformationMatrix *= glm::translate(Mat4(1.0f), m_Translation);
 
-		if (m_Rotation.x != 0.0f)
-			m_TransformationMatrix *= glm::rotate(Mat4(1.0f), m_Rotation.x, Vec3(1.0f, 0.0f, 0.0f));
+		if (m_Rotation != Vec3(0.0f))
+			m_TransformationMatrix *= glm::toMat4(glm::quat(m_Rotation));
 
-		if (m_Rotation.y != 0.0f)
-			m_TransformationMatrix *= glm::rotate(Mat4(1.0f), m_Rotation.y, Vec3(0.0f, 1.0f, 0.0f));
-
-		if (m_Rotation.z != 0.0f)
-			m_TransformationMatrix *= glm::rotate(Mat4(1.0f), m_Rotation.z, Vec3(0.0f, 0.0f, 1.0f));
-
-		if(m_Scale != Vec3(1.0f))
-			m_TransformationMatrix *= glm::scale(Mat4(1.0f), m_Scale);
+		m_TransformationMatrix *= glm::scale(Mat4(1.0f), m_Scale);
 	}
 }
--- SemperEngine/src/SemperEngine/Scene/Components.h
+++ SemperEngine/src/SemperEngine/Scene/Components.h
@@ -24,4 +24,46 @@ namespace SemperEngine
 		std::string name = "Entity";
 		UUID ID;
 	};
+
+	struct TransformComponent
+	{
+		Transform transform;
+
+		TransformComponent(ConstRef<Transform> transfom) : 
+			transform(transform) {}
+		TransformComponent(const TransformComponent &) = default;
+		~TransformComponent() = default;
+
+		operator Transform()
+		{
+			return transform;
+		}
+
+		const std::tuple<Vec3, Vec3, Vec3> GetTranslationRotationScale() const
+		{
+			return std::tuple {
+				transform.GetTranslation(),
+				transform.GetRotation(),
+				transform.GetScale()
+			};
+		}
+
+		void SetTranslation(ConstRef<Vec3> translation)
+		{
+			transform.SetTranslation(translation);
+		}
+		void SetRotation(ConstRef<Vec3> rotation)
+		{
+			transform.SetRotation(rotation);
+		}
+		void SetScale(ConstRef<Vec3> scale)
+		{
+			transform.SetScale(scale);
+		}
+
+		ConstRef<Mat4> GetMatrix() const
+		{
+			return transform.GetTransform();
+		}
+	};
 }
